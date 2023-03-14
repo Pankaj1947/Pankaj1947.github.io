557ca60df3ed1bc601dcdc4c1d467fb41d88f387
Pankaj Kumar Ram <pankajkr885@gmail.com>
2023-03-15 02:56:08 +0530

    MyPortfolio

---
+++ src/pages/Contact.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { links } from '../portfolio';
+import { SocialIcon } from 'react-social-icons';
+import Fade from 'react-reveal/Fade';
+import "./contact.css"
+
+const Contact = () => {
+    return <div className="contact" id="contact">
+        <h1 className="contact-title">
+            <Fade bottom cascade duration={2500}>Reach Out To Me!</Fade>
+        </h1>
+        <div className="icon">
+            {links.map((link) => <div className="icons"> <SocialIcon url={link} target="_blank"
+ /> </div>)}
+        </div>
+        <center>
+            <hr className="style-f" />
+        </center>
+        <p className="copyright">Socializing and Connecting is really important, which leads to great ideas and major breakthroughs. <br />You can contact me at the places mentioned above or at +91 9163474373 .</p>
+    </div >
+}
+
+export default Contact
---
+++ src/portfolio.js
@@ -0,0 +1,160 @@
+//I Would Highly Recommend You To Customize The Theme According To Your Taste.
+export const globalStyles = {
+  "--grad": "linear-gradient(315deg,#48a9fe,#00cdac)", // Use gradients only
+  // "--grad": "linear-gradient(315deg, #48a9fe 0%, #734ae8 74%)", // Use gradients only
+  // "--grad": "linear-gradient(315deg, #06bcfb 0%, #4884ee 74%)",
+  "--txtcolor": "#272341",
+  "--p_color": "#00cdac",
+  "--s_color": "#fff",
+};
+
+// Navigation Bar Name
+export const navbar_name = "Pankaj";
+
+// Main Page
+export const main = {
+  name: "Pankaj Kumar Ram",
+  doamin: "Full Stack Web Developer",
+  resume:
+    "https://drive.google.com/file/d/1MApGt8JYMiDC9LvgTTrhqzV1W2eeAPXx/view",
+};
+
+// AboutMe
+export const aboutme = {
+  imgLink: "Images/PankajHD.png",
+  p1: "Hey, My name is Pankaj Kumar Ram. A passionate individual who always thrive to work on end to end products.",
+  p2: "I'm a Tech Enthusiast who enjoys building awesome website that solves real world problems.",
+  p3: "Innovative, task-driven professional in web-designing, proficient in developing databases, user interfaces, writing & testing Codes, and implementing the features based on the feedback.",
+};
+
+// Skills
+export const whatido = {
+  title: "Full Stack Development",
+
+  details: [
+    "Building responsive website using React & Redux",
+    "Creating backend application in Node & Express",
+    "Building Simple RESTful CRUD APIs",
+    "Core Knowledge of JavaScript Language",
+  ],
+  logos: [
+    {
+      name: "HTML5",
+      iconifyClassName: "vscode-icons:file-type-html", // PS : Use iconify class name
+      id: "1",
+    },
+    {
+      name: "CSS",
+      iconifyClassName: "vscode-icons:file-type-css",
+      id: "2",
+    },
+    {
+      name: "JavaScript",
+      iconifyClassName: "logos:javascript",
+      id: "3",
+    },
+    {
+      name: "ReactJS",
+      iconifyClassName: "vscode-icons:file-type-reactjs",
+      id: "4",
+    },
+    {
+      name: "Redux",
+      iconifyClassName: "logos:redux",
+      id: "5",
+    },
+    {
+      name: "NodeJS",
+      iconifyClassName: "logos:nodejs",
+      id: "6",
+    },
+    {
+      name: "MongoDB",
+      iconifyClassName: "logos:mongodb",
+      id: "7",
+    },
+    {
+      name: "NPM",
+      iconifyClassName: "logos:npm-icon",
+      id: "8",
+    },
+    {
+      name: "VSCode",
+      iconifyClassName: "vscode-icons:file-type-vscode",
+      id: "9",
+    },
+    {
+      name: "PostMan",
+      iconifyClassName: "logos:postman-icon",
+      id: "10",
+    },
+    {
+      name: "Git",
+      iconifyClassName: "bi:git",
+      id: "11",
+    },
+    {
+      name: "Vercel",
+      iconifyClassName: "logos:vercel",
+      id: "12",
+    },
+    {
+      name: "Nextjs",
+      iconifyClassName: "logos:nextjs",
+      id: "13",
+    },
+    {
+      name: "TypeScript",
+      iconifyClassName: "logos:typescript",
+      id: "14",
+    },
+  ],
+};
+
+// Project Details
+export const details = [
+  {
+    id: 1,
+    title: "Clone of PharmEasy",
+    desc: "PharmEasy is a leading online healthcare platform in India that connects patients with local pharmacies and diagnostic centers to provide a wide range of healthcare services. The platform offers a convenient way for patients to order prescription and over-the-counter medications, health supplements, and personal care products from the comfort of their homes.",
+    img: "https://github.com/Saraswati121/imported-existence-8823/raw/main/frontend/public/Home%20Page.png?raw=true'",
+    link: "Tech Stack : React, Redux, ChakraUi, Node.Js, Express, MongoDb.",
+    github: "https://github.com/Saraswati121/imported-existence-8823",
+    deploy: "https://pharmeasy-clone-nem201.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Clone of Boat-LifeStyle",
+    desc: "Boat Lifestyle is a consumer electronics brand that primarily offers audio-related products such as earphones, headphones, speakers, and soundbars. The company was founded in 2016 and is headquartered in Mumbai, India. The Boat Lifestyle website offers a variety of products categorized into different sections such as 'Audio', 'Smart Watches', 'Home Audio', 'Accessories', and 'Shop by Collection'. Customers can browse and purchase products directly from the website.",
+    img: "https://github.com/Pankaj1947/Boat_LifeStyle/blob/main/public/HomePage.png?raw=true",
+    link: "Tech Stack : React, Redux, Css, Javascript",
+    github: "https://github.com/Pankaj1947/Boat_LifeStyle",
+    deploy: "https://boat-lifestyles.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Clone of HealthKart",
+    desc: "HealthKart is an Indian online health and fitness store that offers a wide range of health and wellness products, including dietary supplements, protein supplements, vitamins and minerals, herbal supplements, health drinks, fitness equipment, and accessories. It was founded in 2011 by Sameer Maheshwari and Prashant Tandon and is headquartered in Gurugram, India.",
+    img: "Images/HealthKart.png",
+    link: "Tech Stack : HTML, Css, Javascript",
+    github: "https://github.com/hlv-kakashi/HEALTHKART",
+    deploy: "https://health-kart.netlify.app/",
+  },
+  {
+    id: 4,
+    title: "Weather Forecast",
+    desc: "A scientific yet simple approach to weather forecast built with HTML, CSS & JavaScript using openweather api.",
+    img: "https://github.com/Pankaj1947/Weather-Forcast/blob/main/Weather%20Forecast.png?raw=true",
+    github: "https://github.com/Pankaj1947/Weather-Forcast",
+    link: "Tech Stack : HTML, Css, Javascript",
+    deploy: "https://weather-forcastt.netlify.app/",
+  },
+];
+
+// Social Media Links
+export const links = [
+  "https://www.linkedin.com/in/pankaj-kumar-ram-639437190/",
+  "https://github.com/Pankaj1947",
+
+  "mailto:pankajkr885@gmail.com",
+];
